ce806e8a6c5717c36c376f869cf23a1f5446c956
Md. Zahid Hasan <mrzahidxy@gmail.com>
2026-06-11 22:43:10 +0600

    feat(app): integrate Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,21 @@ import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import { PreferredTeamProvider } from "../components/PreferredTeamContext";
 import { ThemeProvider } from "../components/ThemeContext";
-import NProgress from 'nprogress';
+import NProgress from "nprogress";
 import "nprogress/nprogress.css";
-import Router from 'next/router';
+import Router from "next/router";
+import { Analytics } from "@vercel/analytics/react";
 
 NProgress.configure({
   minimum: 0.3,
-  easing: 'ease',
+  easing: "ease",
   speed: 800,
   showSpinner: false,
-  
 });
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -24,6 +24,7 @@ function MyApp({ Component, pageProps }) {
       <PreferredTeamProvider>
         <Navbar />
         <Component {...pageProps} />
+        <Analytics />
       </PreferredTeamProvider>
     </ThemeProvider>
   );
